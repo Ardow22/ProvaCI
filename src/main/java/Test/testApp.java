80d22aed61661496f9aa5d4e4c1cfa42a532e87b
Ardow22 <apache.rb6@gmail.com>
2024-07-30 00:02:30 +0200

    First commit

---
+++ src/main/java/Test/testApp.java
@@ -0,0 +1,18 @@
+package Test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import logic.App;
+
+
+
+public class testApp {
+
+
+	    @Test
+	    public void testResult()   {
+	        assertEquals(1, App.result());
+	    }
+	}
+
---
+++ src/main/java/logic/App.java
@@ -0,0 +1,12 @@
+package logic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    
+	public static int result() {
+        return 1;
+    }
+}
